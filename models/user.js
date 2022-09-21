5bd3591146545e3607b99991c9690083b42fea8f
Siddhesh199 <siddheshsalaskar188@gmail.com>
2022-09-21 19:37:22 +0530

    server deployment

---
+++ models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const { productSchema } = require("./product");
+
+const userSchema = mongoose.Schema({
+  username: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+  email: {
+    required: true,
+    type: String,
+    trim: true,
+    validate: {
+      validator: (value) => {
+        const re =
+          /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return value.match(re);
+      },
+      message: "Please enter a valid email address",
+    },
+  },
+  password: {
+    required: true,
+    type: String,
+    validate: {
+      validator: (value) => {
+        return value.length > 6;
+      },
+      message: "Password must be atleast 7 characters long",
+    },
+  },
+  address: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "user",
+  },
+  cart: [
+    {
+      product: productSchema,
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      exists: {
+        type: Boolean,
+        required: true,
+      },
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
